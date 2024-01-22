381d48dd319dc25a96f00c76c497370a64075681
Dawid <musiald20@gmail.com>
2024-01-22 10:50:38 +0100

    commiting changes

--- config/config.py
+++ config/config.py
@@ -1,3 +1,3 @@
 # config/config.py
-DISCORD_BOT_TOKEN = 'MTE3NzkyMjc2NzE2NTg1NzgxMg.G1uytU.uSpfehd4QYcu4Neqlso6AWaTteBkrDWKI75lSM'
-OPENAI_API_KEY = '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'
+DISCORD_BOT_TOKEN = 'secret'
+OPENAI_API_KEY = 'secret
